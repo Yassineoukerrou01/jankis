50a8f38b3215a1065a1e55940f3c8c506935eea3
Yassine Oukerrou <Yassine@gmail.com>
2025-11-22 12:44:40 +0100

    first commit

--- DeRien.java
+++ DeRien.java
@@ -1,5 +1,5 @@
 public class DeRien {
     public static void main(String[] args) {
-        System.out.println("DeRien");
+        System.out.println("DeRien ;");
     }
 }
--- Merci.java
+++ Merci.java
@@ -1,5 +1,5 @@
 public class Merci {
     public static void main(String[] args) {
-        System.out.println("merci");
+        System.out.println("merci ;");
     }
 }
